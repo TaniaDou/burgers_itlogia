6433d93f445c60d02ab9958bfe469463ff1abdfa
TaniDou <t.doump@gmail.com>
2024-02-20 13:46:08 +0300

    feat: add scroll in TS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'burgers';
+  scrollTo(target: HTMLElement) {
+    target.scrollIntoView({behavior: "smooth"});
+  }
 }
